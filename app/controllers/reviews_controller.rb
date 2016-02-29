14dfaafcae5e7f85c773424f394e481d51517cc6
zibs <elizibin@gmail.com>
2016-02-29 11:15:08 -0800

    Ajaxify Reviews. Add format block in Reviews Controller Create partials for javascript execution, and review listings. Allow form to be data-remote using remote: true

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,16 +3,20 @@ class ReviewsController < ApplicationController
   def create
     @product = Product.find(params[:product_id]) #found through routes params
     # @review = Review.new(review_params) # just instanstiated
-    @review = Review.new
+    @review = Review.new(review_params)
     @review.product = @product
     # @review.product_id = @product.id #this sets the foreign_key on the review to be associated with the product
     # @review = Product.find(params[:product_id]).review.new(review_params)
-    if @review.save(review_params)
-      redirect_to product_path(@product), flash: { success: "Review created successfully"}
-    else
-      # can just render the template associated here...but how does it find the id? we already have the id
-      render "products/show"
-      # render json: params
+    respond_to do |format|
+      if @review.save
+        format.html { redirect_to product_path((@product), flash: { success: "Review created successfully"}) }
+        format.js { render :create_success }
+      else
+        # can just render the template associated here...but how does it find the id? we already have the id
+        format.html { render "products/show" }
+        format.js { render js: "alert('error happened');" }
+        # render json: params
+      end
     end
   end
 
